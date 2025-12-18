e426ac46ae0a1e07477028645f7f5f41160dde0f
João Victor <168527020+olegarioojv@users.noreply.github.com>
2025-12-17 21:39:58 -0300

    q2

--- cypress/cypress/e2e/login.cy.js
+++ cypress/cypress/e2e/login.cy.js
@@ -1,6 +1,9 @@
 describe("Login", () => {
-  it("Realizar login com sucesso", () => {
+  beforeEach(() => {
     cy.visit("https://www.saucedemo.com/");
+  });
+
+  it("Realizar login com sucesso", () => {
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
@@ -9,7 +12,6 @@ describe("Login", () => {
   });
 
   it("Testar o carrinho de compras", () => {
-    cy.visit("https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
@@ -20,7 +22,6 @@ describe("Login", () => {
   });
 
   it("Fazer checkout", () => {
-    cy.visit("https://www.saucedemo.com/");
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
@@ -36,8 +37,33 @@ describe("Login", () => {
     cy.get('[data-test="continue"]').click();
 
     cy.get('[data-test="finish"]').click();
-    
+
     cy.get('[data-test="back-to-products"]').click();
 
+    cy.url().should("include", "/inventory.html");
+
+    cy.wait(2000);
+    cy.scrollTo("bottom");
+    cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+    cy.get(
+      '[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]'
+    ).click();
+    cy.scrollTo("top");
+
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.get('[data-test="checkout"]').click();
+
+    cy.get('[data-test="firstName"]').type("João Victor");
+    cy.get('[data-test="lastName"]').type("Olegário");
+    cy.get('[data-test="postalCode"]').type("9832");
+
+    cy.get('[data-test="continue"]').click();
+
+    cy.get('[data-test="finish"]').click();
+
+    cy.get('[data-test="back-to-products"]').click();
+
+    cy.url().should("include", "/inventory.html");
   });
 });
